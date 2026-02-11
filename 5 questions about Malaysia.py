0e2e98dd06c977063709b06a2733cab531982af5
Matt32609 <vthematt@gmail.com>
2026-02-11 10:51:13 +0800

    Add files via upload

--- 5 questions about Malaysia.py
+++ 5 questions about Malaysia.py
@@ -78,7 +78,7 @@ def question_4():
 question_4()
 
 def question_5():
-    answer = input("In Kuala Lumpur , there are 2 aerodrome airports that are active . List the districts located for the 2 airports without including the word district in your answer").strip().lower()
+    answer = input("In the Klang Valley , there are 2 aerodrome airports that are active . List the districts located for the 2 airports without including the word district in your answer").strip().lower()
     print(answer)
 
     if "sepang" in answer and "petaling" in answer:
