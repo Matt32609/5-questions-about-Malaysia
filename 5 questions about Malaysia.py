1eef188657148aee918a9537c103e4a4fdedf549
Matt32609 <vthematt@gmail.com>
2026-02-11 18:54:27 +0800

    Add files via upload

--- 5 questions about Malaysia.py
+++ 5 questions about Malaysia.py
@@ -107,6 +107,7 @@ question_5()
 def end_sequence():
     print("Congrats, you really have the knowledge to do this!")
     redo = input("Do you wish to try again? ").strip().lower()
+    
     if redo == "yes":
         print("Starting over from Question 1...")
         return True 
